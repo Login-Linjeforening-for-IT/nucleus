ae264f97ace0b37dc1cc5e344f5122a111d9cd03
Eirik Hanasand <eirik.hanasand@gmail.com>
2023-01-13 01:46:54 +0100

    Fixed mail error handling

--- shared/sharedComponents.js
+++ shared/sharedComponents.js
@@ -13,7 +13,8 @@ import {
     Switch, 
     TouchableOpacity,
     Image,
-    Linking
+    Linking,
+    Alert
 } from 'react-native';
 import { GS } from '../styles/globalStyles';
 import FetchColor from '../styles/fetchTheme';
@@ -87,7 +88,7 @@ export function Kontakt() { //Contact info
         <Text style={{...T.centered15, color: FetchColor(theme, 'TEXTCOLOR')}}>{lang ? 'Bygg A, rom 155': 'Building A, room 155'}</Text>
         <Text style={{...T.centered15, color: FetchColor(theme, 'TEXTCOLOR')}}>2815 GJØVIK</Text>
         <Text/>
-        <Text style={{...T.orangeCentered15, color: FetchColor(theme, 'ORANGE')}} onPress={() => Linking.openURL('mailto:kontakt@login.no')}>kontakt@login.no</Text>
+        <Text style={{...T.orangeCentered15, color: FetchColor(theme, 'ORANGE')}} onPress={async() => {Linking.openURL('mailto:kontakt@login.no').catch(() => lang ? Alert.alert('Kunne ikke åpne mail!', 'Mail: kontakt@login.no'):Alert.alert('Could not open mail!', 'Reach us at kontakt@login.no'))}}>kontakt@login.no</Text>
         <Text/>{Space(12)}
     </View> 
     )
