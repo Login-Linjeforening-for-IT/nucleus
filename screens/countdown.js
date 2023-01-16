1c56c5a80a00b45dd29f0dd2d5646343d47acb7e
Eirik Hanasand <eirik.hanasand@gmail.com>
2023-01-16 20:05:35 +0100

    Nucleus Initiated

--- screens/countdown.js
+++ screens/countdown.js
@@ -2,6 +2,7 @@ import { GS } from '../styles/globalStyles'
 import React, {useEffect, useState} from 'react';
 import { T } from '../styles/text'
 import { StatusBar } from 'expo-status-bar';
+import { Space } from '../shared/sharedComponents';
 import { NotificationDelay } from '../shared/eventComponents/notificationDelay';
 import { 
   Text, 
@@ -14,8 +15,9 @@ return(
   <View>
       <StatusBar hidden={true}/>
       <View style={{...GS.content, backgroundColor: 'black'}}>
-          <View style={{...T.centeredBold20, flex: 1, justifyContent: 'center'}}>
+          <View style={{flex: 1, justifyContent: 'center'}}>
             <Text style={{...T.centeredBold25, color: 'white'}}>Nucleus</Text>
+            {Space(20)}
             <Text style={{...T.centeredBold25, color: 'white'}}><Countdown props={startt="2023-02-11T00:00:00Z"}/></Text>
           </View>
       </View>    
@@ -32,10 +34,10 @@ function Countdown(props) {
     }, 1000);
     return () => clearInterval(interval);
   }, [timer]);
-  var days = Math.floor(timer/86400) == 0 ? '':Math.floor(timer/86400)
-  var hour = 1 + Math.floor((timer%86400)/3600) == 0 ? '':Math.floor(timer/86400)
-  var minutes = Math.floor(((timer%86400)%3600)/60) == 0 ? '':Math.floor(((timer%86400)%3600)/60)
-  var seconds = ((timer%86400)%3600)%60 == 0 ? '':((timer%86400)%3600)%60
-  var countdown = days + ' ' + hour + ' ' + minutes + ' ' + seconds
+  var days = Math.floor(timer/86400) == 0 ? '':Math.floor(timer/86400) + 'd '
+  var hour = 1 + Math.floor((timer%86400)/3600) == 0 ? '':1 + Math.floor((timer%86400)/3600) + 't '
+  var minutes = Math.floor(((timer%86400)%3600)/60) == 0 ? '':Math.floor(((timer%86400)%3600)/60) + 'm '
+  var seconds = ((timer%86400)%3600)%60 == 0 ? '':((timer%86400)%3600)%60 + 's '
+  var countdown = days + hour + minutes + seconds
   return countdown;
 }
