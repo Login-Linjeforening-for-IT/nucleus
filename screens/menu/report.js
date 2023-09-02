a8f88d2bd090e3ca6c7b54a3af1c8bce4dce72d2
eirikhanasand <eirik.hanasand@gmail.com>
2023-09-02 20:20:27 +0200

    Updated tag

--- screens/menu/report.js
+++ screens/menu/report.js
@@ -40,7 +40,7 @@ export default function ReportScreen( { navigation }) {
   
   const sendForm = () => {
     if (data.name === data.name) {
-      lang ? Alert.alert('Denne funksjonen kommer snart', 'Midlertidig løsning:\nMail: kontakt@login.no\nDiscord: Axe#9595') : Alert.alert('This function is coming soon.', 'Temporary solution:\nMail: kontakt@login.no\nDiscord: Axe#9595') // Takk for beskjed. / Thanks for letting us know.
+      lang ? Alert.alert('Denne funksjonen kommer snart', 'Midlertidig løsning:\nMail: kontakt@login.no\nDiscord: eirikhanasand') : Alert.alert('This function is coming soon.', 'Temporary solution:\nMail: kontakt@login.no\nDiscord: eirikhanasand') // Takk for beskjed. / Thanks for letting us know.
     } else {
       lang ? Alert.alert('Feil! Vennligst send varslingen som anonym epost til kontakt@login.no') : Alert.alert('Error! Please send the report as an anonymous email to kontakt@login.no')
     }
