27c550108f85d6224e91c6f31be5409b6c86a4b6
Gjermundhp02 <gjermund.pedersen@gmail.com>
2023-09-02 21:29:59 +0200

    Fixed back button not returning to last screen

--- routes/stack.js
+++ routes/stack.js
@@ -20,7 +20,7 @@ import MenuScreen from '../screens/menu'; // Menu
 function Navigator() { // Declares Navigator, wraps in container and declares all navigation routes
     return (
         <NavigationContainer>
-            <Tab.Navigator screenOptions={
+            <Tab.Navigator backBehavior='history' initialRouteName='EventScreen' screenOptions={
                 {headerShown: false}
             }>
                 <Tab.Screen name='EventScreen' options={ { tabBarStyle: { display: "none" } } } component={EventScreen}/>
