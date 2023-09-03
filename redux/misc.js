dd0a01ee15ed231ef7b58351313f4f4e0236fbab
eirikhanasand <eirik.hanasand@gmail.com>
2023-09-04 01:26:42 +0200

    Removed dead code

--- redux/misc.js
+++ redux/misc.js
@@ -4,22 +4,14 @@ export const MiscSlice = createSlice({              // Declares Misc Slice
     name: 'misc',                                   // Slice name
     initialState: {                                 // Initial state
         calendarID: null,                           // Null till the user makes a calendar
-        remoteNavigate: {},                         // Object to use for the navigation from a push notification
-        hasNavigated: false                         // Tracks if the user has navigated to the page
     },
     reducers: {                                     // Declares slice reducer
         setCalendarID: (state, action) => {         // Function for setting the calendar ID
             state.calendarID = action.payload       // Sets the calendarID to the payload
-        },
-        setRemoteNavigate: (state, action) => {     // Function for setting the remote object
-            state.remoteNavigate = action.payload   // Sets the remoteNavigate object equal to the payload
-        },
-        setHasNavigated: (state, action) => {       // Function for setting the remote object
-            state.remoteNavigate = action.payload   // Sets the remoteNavigate object equal to the payload
         }
     }
 })
 
-export const { setCalendarID, setRemoteNavigate, setHasNavigated } = MiscSlice.actions  // Exports redurcers
+export const { setCalendarID } = MiscSlice.actions  // Exports redurcers
 
 export default MiscSlice.reducer                    // Exports the misc slice
