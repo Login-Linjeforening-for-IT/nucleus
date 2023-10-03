5c2ca4c4fd28d2abfb2f2248f2ecb680fddef6af
eirikhanasand <eirik.hanasand@gmail.com>
2023-10-03 05:02:07 +0200

    Reverted, import not allowed

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,9 @@
-import { getDefaultConfig } from 'expo/metro-config'
+const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
+    // Enable CSS support.
     isCSSEnabled: true,
 })
 
-export default config
+module.exports = config
