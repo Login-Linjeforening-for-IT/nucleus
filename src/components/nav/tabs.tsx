932c846b3f3908db9dee9eb25a785998b46b9dae
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-02-09 16:52:57 +0100

    Added animation

--- src/components/nav/tabs.tsx
+++ src/components/nav/tabs.tsx
@@ -7,9 +7,9 @@ import EventScreen from "@screens/event"
 import MenuScreen from "@screens/menu"
 import AdScreen from "@screens/ads"
 import React from "react"
-import { Image, View } from "react-native"
+import { Animated, Dimensions, Image, View } from "react-native"
 import MS from "@styles/menuStyles"
-import { createStackNavigator } from "@react-navigation/stack"
+import { StackCardInterpolatedStyle, StackCardInterpolationProps, StackCardStyleInterpolator, createStackNavigator } from "@react-navigation/stack"
 import TagInfo from "@components/shared/tagInfo"
 
 // Declares Tab to equal CBTN function
@@ -91,40 +91,32 @@ function Tabs(){
  * @returns Application with navigation
  */
 export default function Navigator(): JSX.Element {
-    const { isDark } = useSelector((state: ReduxState) => state.theme )
-
-    const screens = [
-        {
-            name: "EventScreen",
-            component: EventScreen,
-            focusedIcon: require("@assets/menu/calendar-orange.png"),
-            icon: isDark
-                ? require("@assets/menu/calendar777.png")
-                : require("@assets/menu/calendar-black.png")
+    const animate = ({current}: StackCardInterpolationProps): StackCardInterpolatedStyle=>({
+        cardStyle: {
+            transform: [{translateY: current.progress.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [200, 0],
+                    extrapolate: 'clamp',
+                })
+                },
+            ],
         },
-        {
-            name: "AdScreen",
-            component: AdScreen,
-            focusedIcon: require("@assets/menu/business-orange.png"),
-            icon: isDark
-                ? require("@assets/menu/business.png")
-                : require("@assets/menu/business-black.png")
-        },
-        {
-            name: "MenuScreen",
-            component: MenuScreen,
-            focusedIcon: require("@assets/menu/menu-orange.png"),
-            icon: isDark
-                ? require("@assets/menu/menu.png")
-                : require("@assets/menu/menu-black.png")
+        overlayStyle: {
+            backgroundcolor: 'black',
+            opacity: current.progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 0.4],
+                extrapolate: 'clamp',
+            })
         }
-    ]
+    })
+    
 
     return (
         <NavigationContainer>
             <Root.Navigator screenOptions={{headerShown: false}}>
                 <Root.Screen name="Tabs" component={Tabs}/>
-                <Root.Screen name="InfoModal" options={{presentation: 'transparentModal', cardOverlayEnabled: true}} component={TagInfo}/>
+                <Root.Screen name="InfoModal" options={{presentation: 'transparentModal', cardOverlayEnabled: true, cardStyleInterpolator: animate}} component={TagInfo}/>
             </Root.Navigator>
         </NavigationContainer>
     )
