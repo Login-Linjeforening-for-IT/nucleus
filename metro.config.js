d7763d1e0994e2bfa4627fdac92da491897cbb65
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-04 01:37:25 +0200

    Fixed metro config

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,18 @@
-const { getDefaultConfig } = require('expo/metro-config')
+// Imports default configuration function from '@react-native/metro-config'
+const { getDefaultConfig } = require('@react-native/metro-config')
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-    // Enable CSS support.
-    isCSSEnabled: true,
-})
+// Imports the Expo Metro configuration function
+const { getDefaultConfig: getExpoDefaultConfig } = require('expo/metro-config')
 
-module.exports = config
+/** @type {import('metro-config').MetroConfig} */
+// Merges the configurations from both '@react-native/metro-config' and Expo
+const config = getExpoDefaultConfig(__dirname)
+
+// Enables css support
+config.resolver.sourceExts.push('css')
+
+// Exports the merged configuration
+module.exports = {
+    ...getDefaultConfig(__dirname),
+    ...config,
+}
