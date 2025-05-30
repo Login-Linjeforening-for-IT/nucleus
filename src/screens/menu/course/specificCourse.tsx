e90bad92957f886ca9996b5ff952f0da1d78de3d
eirikhanasand <eirik.hanasand@gmail.com>
2025-05-30 21:27:52 +0200

    Fixed bugs

--- src/screens/menu/course/specificCourse.tsx
+++ src/screens/menu/course/specificCourse.tsx
@@ -4,10 +4,12 @@ import Parent from "@components/shared/parent"
 import { setLocalTitle } from "@redux/misc"
 import { getCourse } from "@utils/course"
 import { useCallback, useEffect, useState } from "react"
-import { Dimensions, Platform, RefreshControl, Text } from "react-native"
+import { Dimensions, Platform, RefreshControl, Text, View } from "react-native"
 import Swipeable from "@components/course/swipeable"
 import { ScrollView } from "react-native-gesture-handler"
 import T from "@styles/text"
+import Markdown from "@components/course/markdown"
+import Space from "@components/shared/utils"
 
 export default function SpecificCourseScreen({ route }: MenuProps<"SpecificCourseScreen">): JSX.Element {
     const { theme } = useSelector((state: ReduxState) => state.theme)
@@ -19,7 +21,7 @@ export default function SpecificCourseScreen({ route }: MenuProps<"SpecificCours
     const height = Dimensions.get("window").height
 
     if (route.params.courseID !== localTitle?.title) {
-        dispatch(setLocalTitle({title: route.params.courseID, screen: "SpecificCourseScreen"}))
+        dispatch(setLocalTitle({ title: route.params.courseID, screen: "SpecificCourseScreen" }))
     }
 
     async function fetchCourse() {
@@ -33,14 +35,14 @@ export default function SpecificCourseScreen({ route }: MenuProps<"SpecificCours
 
     useEffect(() => {
         (async () => {
-           await fetchCourse()
+            await fetchCourse()
         })()
     }, [])
 
     const onRefresh = useCallback(async () => {
         setRefresh(true)
         const course = await fetchCourse()
-        
+
         if (course) {
             setClicked([])
             setRefresh(false)
@@ -59,31 +61,51 @@ export default function SpecificCourseScreen({ route }: MenuProps<"SpecificCours
         if (height > 700 && height < 800) {
             return 17.5
         }
-        
+
         if (height > 800 && height < 900) {
             return 40
         }
-        
+
         return undefined
     }
-        
+
     return (
         <Parent paddingHorizontal={-1}>
             <ScrollView
-                showsVerticalScrollIndicator={false} 
+                showsVerticalScrollIndicator={false}
                 scrollEventThrottle={100}
-                style={{paddingVertical: 10, bottom: 10, paddingTop: Platform.OS === 'ios' ? undefined : paddingtop()}}
+                style={{ paddingVertical: 10, bottom: 10, paddingTop: Platform.OS === 'ios' ? undefined : paddingtop() }}
             >
-            <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
-            {typeof course === 'string' 
-                ? <Text style={{ ...T.text18, color: theme.textColor }}>{course}</Text> 
-                : <Swipeable
-                    course={course} 
-                    clicked={clicked} 
-                    setClicked={setClicked} 
-                />
-            }
+                <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
+                {typeof course === 'string'
+                    ? <Text style={{ ...T.text18, color: theme.textColor }}>{course}</Text>
+                    : course.cards.length ? <Swipeable
+                        course={course}
+                        clicked={clicked}
+                        setClicked={setClicked}
+                    /> : <Study course={course} />
+                }
             </ScrollView>
         </Parent>
+    )
+}
+
+function Study({ course }: { course: Course }) {
+    const { lang } = useSelector((state: ReduxState) => state.lang)
+
+    function formatText(text: string) {
+        return text.replaceAll('<br></br>', '\n').replaceAll('<br>', '\n')
+    }
+
+    const initialText = formatText(course.textUnreviewed.join('\n'))
+    const text = initialText.length ? initialText : lang 
+        ? 'Dette emnet er ikke flervalg, men det finnes ingen notater enda. Dette emnet må studeres på egenhånd. Kanskje du kan hjelpe andre med å legge inn notatene dine på nettsiden så de blir tilgjengelige her?'
+        : 'This course is not multiple choice, and no study notes exist yet. This course must be studied by reviewing the available course material. Maybe you can help others by adding your notes on the website so they become available here?'
+
+    return (
+        <View>
+            <Markdown text={text} />
+            <Space height={text.length / 100} />
+        </View>
     )
 }
