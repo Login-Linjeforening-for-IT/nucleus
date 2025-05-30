b92f92c4caae37003b71671909bb39f1ffdd9060
gjermundhp02 <gjermund.pedersen@gmail.com>
2025-05-30 20:08:28 +0200

    Added auto increment developer version code

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,87 @@ import { ExpoConfig, ConfigContext } from '@expo/config'
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
     ...config,
-    name: 'Login',
-    slug: 'Login',
+    owner: "loginapp",
+    name: "Login",
+    slug: "Login",
+    version: "2.1.10",
+    orientation: "portrait",
+    icon: "./public/assets/logo/icon.png",
+    userInterfaceStyle: "dark",
+    splash: {
+        image: "./public/assets/logo/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#000000"
+    },
+    updates: {
+        fallbackToCacheTimeout: 0,
+        url: "https://u.expo.dev/952a1914-0c53-43e7-b64e-8daab0b3a435"
+    },
+    plugins: [
+        ["@react-native-firebase/app"],
+        [
+            "expo-calendar",
+            {
+                calendarPermission: "Login wants to save events to your calendar."
+            }
+        ],
+        [
+            "expo-build-properties",
+            {
+                ios: {
+                    useFrameworks: "static"
+                }
+            }
+        ],
+        [
+            "expo-image-picker",
+            {
+                photosPermission: "Login needs photo access if you want a profile picture.",
+                cameraPermission: "Login needs camera access if you want to take a selfie."
+            }
+        ]
+    ],
+    assetBundlePatterns: [
+        "**/*"
+    ],
     ios: {
-        googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST,
-        ...config.ios
+        supportsTablet: true,
+        bundleIdentifier: "com.eirikhanasand.Login",
+        buildNumber: config.ios?.buildNumber,
+        googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST || "./GoogleService-Info.plist",
+        infoPlist: {
+            UIBackgroundModes: [
+                "fetch",
+                "remote-notification"
+            ]
+        }
     },
     android: {
-        googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-        ...config.android
+        adaptiveIcon: {
+            foregroundImage: "./public/assets/logo/adaptiveIcon.png",
+            backgroundColor: "#000000"
+        },
+        package: "com.login.Login",
+        googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json",
+        versionCode: config.android?.versionCode,
+        permissions: [
+            "INTERNET",
+            "NOTIFICATIONS",
+            "PUSH_NOTIFICATIONS",
+            "READ_CALENDAR",
+            "WRITE_CALENDAR"
+        ],
+        softwareKeyboardLayoutMode: "pan"
+    },
+    web: {
+        favicon: "./public/assets/logo/favicon.ico"
+    },
+    extra: {
+        eas: {
+            projectId: "952a1914-0c53-43e7-b64e-8daab0b3a435"
+        }
+    },
+    runtimeVersion: {
+        policy: "sdkVersion"
     }
 });
