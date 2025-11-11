adfee2511ecd6bc79f81cd75619a6b723d9ec084
eirikhanasand <eirik.hanasand@gmail.com>
2025-11-11 13:14:02 +0100

    Updated version

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     owner: "loginapp",
     name: "Login",
     slug: "Login",
-    version: "2.3.2",
+    version: "2.4.0",
     orientation: "portrait",
     icon: "./public/assets/logo/icon.png",
     userInterfaceStyle: "dark",
